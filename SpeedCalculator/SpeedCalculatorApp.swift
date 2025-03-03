1eae93524e02769967a90cfdf40410164727dac0
Connor Engel <cengel@lcs.on.ca>
2025-03-03 14:24:36 -0500

    Initial Commit

---
+++ SpeedCalculator/SpeedCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedCalculatorApp.swift
+//  SpeedCalculator
+//
+//  Created by Connor Engel on 2025-03-03.
+//
+
+import SwiftUI
+
+@main
+struct SpeedCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
